5f4df9a7868213eede407c407cd553cf99b9e8e1
Silverfox088 <mindi.h@frc1073.org>
2023-09-06 19:59:44 -0400

    Added comments for the TeleopDriveCommand

--- kitbot2023/src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ kitbot2023/src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -29,6 +29,8 @@ public class TeleopDriveCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    // Uses the setPower method from the Drivetrain subsystem
+    // Uses percent input from the joysticks which sets the speed [between -1 to 1] to the motors
     m_drivetrain.setPower(m_OI.getDriverLeftY(), m_OI.getDriverRightY());
 
   }
@@ -36,6 +38,7 @@ public class TeleopDriveCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    // Sets motor speeds to 0
     m_drivetrain.setPower(0, 0);
   }
 
