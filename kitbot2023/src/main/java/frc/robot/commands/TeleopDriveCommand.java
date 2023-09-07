903ee1e37208c83f487e0deba51426149b6966fd
Oli <gstrmllr24@gmail.com>
2023-09-06 20:25:13 -0400

    Commented OI

--- kitbot2023/src/main/java/frc/robot/commands/TeleopDriveCommand.java
+++ kitbot2023/src/main/java/frc/robot/commands/TeleopDriveCommand.java
@@ -22,11 +22,11 @@ public class TeleopDriveCommand extends CommandBase {
     addRequirements(m_drivetrain);
   }
 
-  // Called when the command is initially scheduled.
+  /* Called when the command is initially scheduled. */
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
+  /* Called every time the scheduler runs while the command is scheduled. */
   @Override
   public void execute() {
     // Uses the setPower method from the Drivetrain subsystem
@@ -34,14 +34,14 @@ public class TeleopDriveCommand extends CommandBase {
     m_drivetrain.setPower(m_OI.getDriverLeftY(), m_OI.getDriverRightY());
   }
 
-  // Called once the command ends or is interrupted.
+  /* Called once the command ends or is interrupted. */
   @Override
   public void end(boolean interrupted) {
     // Sets motor speeds to 0, so it stops when the command ends
     m_drivetrain.setPower(0, 0);
   }
 
-  // Returns true when the command should end.
+  /* Returns true when the command should end. */
   @Override
   public boolean isFinished() {
     return false;
