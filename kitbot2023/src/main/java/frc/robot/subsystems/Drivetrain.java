6949a4ec59179717a1a3c69689e51eb609b42fc1
Oli <gstrmllr24@gmail.com>
2023-09-06 20:06:48 -0400

    Cleaned up and commented drivetrain

--- kitbot2023/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ kitbot2023/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,80 +4,46 @@
 
 package frc.robot.subsystems;
 
-/*to get import for Rev hardware use this json file: 
+/* To get import for Rev hardware use this json file: 
 https://software-metadata.revrobotics.com/REVLib-2023.json
 WPI button -> manage vendor libraries -> install new library (online) */
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-
-
 public class Drivetrain extends SubsystemBase {
-  
-  NetworkTable drivetrainTable;
-  NetworkTableEntry pEntry;
-  NetworkTableEntry iEntry;
-  NetworkTableEntry dEntry;
-  NetworkTableEntry fEntry;
-  NetworkTableEntry updateButton;
- 
+  // Declares the motors' variables
   private Spark leftMotorLeader;
   private Spark leftMotorFollower;
   private Spark rightMotorLeader;
   private Spark rightMotorFollower;
-  
-  private double kP = 0.15 * 0.75;
-  private double kI = 0.002 * 0;
-  private double kD = 0.0;
-  private double kF = 0.052;
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
-    drivetrainTable = NetworkTableInstance.getDefault().getTable("Drivetrain");
-    pEntry = drivetrainTable.getEntry("kP");
-    iEntry = drivetrainTable.getEntry("kI");
-    dEntry = drivetrainTable.getEntry("kD");
-    fEntry = drivetrainTable.getEntry("kF");
-
-    updateButton = drivetrainTable.getEntry("update constants");
-
-    updateButton.setBoolean(true);
-    pEntry.setDouble(kP);
-    iEntry.setDouble(kI);
-    dEntry.setDouble(kD);
-    fEntry.setDouble(kF);
-
-
+    // Initializes the motors to Spark motor objects with pwm channels corresponding to where they're plugged in on the robot
     rightMotorLeader = new Spark(0);
     rightMotorFollower = new Spark(1);
     leftMotorLeader = new Spark(2);
     leftMotorFollower = new Spark(3);
-    // leftMotorFollower.follow(leftMotorLeader);
-    // rightMotorFollower.follow(rightMotorLeader);
   }
 
+  /* Called once per scheduler run */
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    // We're not doing anything here so that it doesn't conflict with our commands!
   }
 
+  /* Sets the motors to given speeds for the left and right sides of the robot */
   public void setPower(double leftPower, double rightPower)
   {
-    leftMotorLeader.set(-leftPower*.5);
-    leftMotorFollower.set(-leftPower*.5);
+    // The leader and follower *must* be set to the same value because they're physically connected on the robot
+    // (We do not want to tear the robot apart by making them go in different directions)
+    leftMotorLeader.set(-leftPower*.5); // Because the motors are facing in different directions, we reverse the input on one side
+    leftMotorFollower.set(-leftPower*.5); // We're also setting them to half speed (comp robots should go fast but test robots don't need to)
     rightMotorLeader.set(rightPower*.5);
     rightMotorFollower.set(rightPower*5);
-    SmartDashboard.putNumber("Right Power", rightPower);
+    SmartDashboard.putNumber("Right Power", rightPower); // Prints the values to shuffleboard for debugging
     SmartDashboard.putNumber("Left Power", leftPower);
   }
-
-  // public void drive(double left, double right){
-  //   drivetrain.tankDrive(left, right);
-  // }
 }
