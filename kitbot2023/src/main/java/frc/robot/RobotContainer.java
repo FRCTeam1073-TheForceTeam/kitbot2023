153213def9e39048b5c356622ae5bab91b73f8ac
VittorioMasoni <146299194+VittorioMasoni@users.noreply.github.com>
2023-11-08 20:46:48 -0500

    Code
    Made sequential command, right wheel spin and a little bit of left wheel spin.

--- kitbot2023/src/main/java/frc/robot/RobotContainer.java
+++ kitbot2023/src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,13 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.SpinAuto;
 import frc.robot.commands.TeleopDriveCommand;
+import frc.robot.commands.Vittorio;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.OI;
 
@@ -20,13 +25,22 @@ public class RobotContainer {
   private final Drivetrain m_drivetrain = new Drivetrain();
   private final OI m_OI = new OI();
   private final TeleopDriveCommand teleopDriveCommand = new TeleopDriveCommand(m_drivetrain, m_OI);
+  private final Vittorio autoCommand = new Vittorio(m_drivetrain);
+  private final SpinAuto spinAuto = new SpinAuto(m_drivetrain);
   // Add new subsystems and commands here, then set their dependencies and add triggers/return commands in autonomous when applicable
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private static final String kNoAuto = "No Autonomous";
+  private static final String kAutoCommand = "Auto Command";
+  private static final String kSpinAuto = "Spin Auto";
 
   /** Creates the container for the robot, which contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Sets default commands for each subsystem when they exist
     m_drivetrain.setDefaultCommand(teleopDriveCommand);
     
+    m_chooser.setDefaultOption("No Autonomous", kNoAuto);
+    m_chooser.addOption("Auto Command", kAutoCommand);
+    m_chooser.addOption("Spin Auto", kSpinAuto);
     configureBindings();
   }
 
@@ -37,7 +51,31 @@ public class RobotContainer {
   
   /** Returns the command to run in autonomous */
   public Command getAutonomousCommand() {
+    switch (m_chooser.getSelected()) {
+      case kNoAuto:
+        return null;
+      case kAutoCommand:
+        return autoCommand;
+      case kSpinAuto:
+      return spinAuto;
+
+    }
     // return Autos.exampleAuto(m_exampleSubsystem);
-    return null;
+    return autoCommand;
+  }
+
+  public Command SequentialCommand() {
+    return new SequentialCommandGroup(
+      new SpinAuto(m_drivetrain),
+      new Vittorio(m_drivetrain),
+      new Vittorio (m_drivetrain),
+      new SpinAuto(m_drivetrain)
+    );
+
+  }
+  public Command ParallelCommandGroup() {
+    return new ParallelCommandGroup();
+
+
   }
 }
