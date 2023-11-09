bc9a5fc5f720f54716520409d5685bb803c08e02
Silverfox088 <82126044+SnowballGus@users.noreply.github.com>
2023-11-08 20:03:10 -0500

    added sequential command group

--- kitbot2023/src/main/java/frc/robot/RobotContainer.java
+++ kitbot2023/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,8 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.AutoDriveCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.Patrick;
 import frc.robot.commands.TeleopDriveCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.OI;
@@ -21,8 +22,7 @@ public class RobotContainer {
   private final Drivetrain m_drivetrain = new Drivetrain();
   private final OI m_OI = new OI();
   private final TeleopDriveCommand teleopDriveCommand = new TeleopDriveCommand(m_drivetrain, m_OI);
-  private final AutoDriveCommand autoDriveCommand = new AutoDriveCommand(m_drivetrain);
-  // Add new subsystems and commands here, then set their dependencies and add triggers/return commands in autonomous when applicable
+    // Add new subsystems and commands here, then set their dependencies and add triggers/return commands in autonomous when applicable
 
   /** Creates the container for the robot, which contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -39,7 +39,14 @@ public class RobotContainer {
   /** Returns the command to run in autonomous */
   public Command getAutonomousCommand() {
     // return Autos.exampleAuto(m_exampleSubsystem);
-    return autoDriveCommand;
+    return sequentialCommand();
+  }
+
+  public Command sequentialCommand(){
+    return new SequentialCommandGroup(
+     new Patrick(m_drivetrain),
+     new Patrick(m_drivetrain)
+    );
   }
 
   public void diagnostics() {
