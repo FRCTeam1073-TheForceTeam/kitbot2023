3aee3fdec4e8474f989d6e7e75242dca7460c1e8
William <william.vanuitert@gmail.com>
2023-11-08 19:49:31 -0500

    Create arjunDrivesCommand.java

---
+++ kitbot2023/src/main/java/frc/robot/commands/arjunDrivesCommand.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class arjunDrivesCommand extends CommandBase {
+  private Drivetrain m_drivetrain;
+  private int counter; 
+
+  /** Creates a new arjunDrivesCommand. */
+  public arjunDrivesCommand(Drivetrain drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = drivetrain;
+    addRequirements(m_drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    counter = 0;
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    counter ++;
+    m_drivetrain.setPower(0.5, 0.3);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drivetrain.setPower(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (counter >= 350)
+    {
+      return true;
+    }
+    return false;
+  }
+}
