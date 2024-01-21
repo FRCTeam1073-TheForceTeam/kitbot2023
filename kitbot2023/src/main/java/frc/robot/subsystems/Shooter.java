6029777bd43de1b91fdaf0c50ca2e279d27f6c67
I v a n <isgn2031@gmail.com>
2024-01-21 16:00:46 -0500

    made it actually work
    man i love it when the docs are clear and concise

--- kitbot2023/src/main/java/frc/robot/subsystems/Shooter.java
+++ kitbot2023/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,40 +4,77 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ControlModeValue;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase 
 {
-  TalonFX topMotor = new TalonFX(0); // TODO: set device id
-  TalonFX bottomMotor = new TalonFX(0); // same thing
+  TalonFX topMotor;
+  TalonFX bottomMotor;
   private double speed;
   private double offset;
+
+  TalonFXConfiguration configs = new TalonFXConfiguration();
+
+  private double p = 0.11;
+  private double i = 0.5;
+  private double d = 0.0001;
 
   /** Creates a new Shooter. */
   public Shooter() 
   {
+    topMotor = new TalonFX(16);
+    bottomMotor = new TalonFX(18);
+
     speed = 0;
     offset = 0;
+    setConfigs();
+  }
+
+  public void setConfigs(){
+    configs.Slot0.kP = p;
+    configs.Slot0.kI = i;
+    configs.Slot0.kD = d;
+    configs.Slot0.kV = 0.12;
+    configs.Voltage.PeakForwardVoltage = 8;
+    configs.Voltage.PeakReverseVoltage = -8;
+
+    configs.Slot1.kP = 5;
+    configs.Slot1.kI = 0.1;
+    configs.Slot1.kD = 0.001;
+
+    configs.TorqueCurrent.PeakForwardTorqueCurrent = 40;
+    configs.TorqueCurrent.PeakReverseTorqueCurrent = -40;
+
+    topMotor.getConfigurator().apply(configs);
+    bottomMotor.getConfigurator().apply(configs);
   }
 
   public void setTopMotorSpeed(double speed)
   {
     topMotor.setControl(new VelocityVoltage(speed));
+    // topMotor.set(speed);
+    // topMotor.setControl(new VelocityVoltage(speed).withSlot(0).withFeedForward(0));
   }
 
   public void setBottomMotorSpeed(double speed)
   {
     bottomMotor.setControl(new VelocityVoltage(speed));
+    // VelocityVoltage velvolt = new VelocityVoltage(speed);
+    // velvolt = velvolt.withAcceleration(1.0);
+    // bottomMotor.setControl(velvolt);
   }
 
   public void setMotorSpeeds(double speed, double offset)
   {
     setTopMotorSpeed(speed);
-    setBottomMotorSpeed(speed - offset);
+    setBottomMotorSpeed(speed + offset);
   }
 
   public void setSpeed(double speed)
@@ -60,17 +97,44 @@ public class Shooter extends SubsystemBase
     return offset;
   }
 
+  public double getP(){
+    return p;
+  }
+
+  public void setP(double p){
+    this.p = p;
+  }
+
+  public double getI(){
+    return i;
+  }
+
+  public void setI(double i){
+    this.i = i;
+  }
+
+  public double getD(){
+    return d;
+  }
+
+  public void setD(double d){
+    this.d = d;
+  }
+
   @Override
   public void initSendable(SendableBuilder builder)
   {
     builder.setSmartDashboardType("Shooter");
     builder.addDoubleProperty("Speed", this::getSpeed, this::setSpeed);
     builder.addDoubleProperty("Offset", this::getOffset, this::setOffset);
+    builder.addDoubleProperty("p", this::getP, this::setP);
+    builder.addDoubleProperty("i", this::getI, this::setI);
+    builder.addDoubleProperty("d", this::getD, this::setD);
   }
 
   @Override
   public void periodic() 
   {
-    setMotorSpeeds(speed, offset);
+    setMotorSpeeds(-speed, offset);
   }
 }
